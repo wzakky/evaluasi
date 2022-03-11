81cf60aa27d4deaba8d3b027b3468fc0f05cbb7f
wildanzakky <wildanzakky125@gmail.com>
2022-03-11 08:58:07 +0700

    1hour

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
-function App() {
+
+export default function App() {
+  const [data, setData] = useState([])
+  const [edit, setEdit] = useState(null)
+
+  const getData = () => {
+    axios.get('http://localhost:3001/trello')
+      .then(hasil => setData(hasil.data))
+  }
+
+  useEffect(() => {
+    getData()
+
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const value = e.target.submit.value
+    axios.post('http://localhost:3001/trello', { name: value, list: [] }).then(() => {
+    getData()
+    e.target.submit.value = ''
+    })
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-5">
+        <h1 className='text-center'>
+          Trello App
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <input name='submit'></input>
+          <button type='submit'>Add</button>
+        </form>
     </div>
   );
 }
 
-export default App;
